57debf8a6db9a138c4444c32ddd355c498770255
Sean Dutton <iceman198@gmail.com>
2021-10-08 10:36:43 -0400

    setup some flask

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dd-voltage-monitor
 Monitor two voltage inputs
+
+# Using Raspberry Pi as Hotspot
+More info found here: https://www.theusabilitypeople.com/article/raspberry-pi-zero-hotspot-control-leds#:~:text=Raspberry%20Pi%20Zero%20as%20Hotspot%20to%20control%20LEDs,Access%20Point%20using%20Hostapd.%20...%20More%20items...%20
---
+++ raspberryPi/src/main.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+import sys;
+import os;
+import time;
+import signal;
+import serial;
+
+from flask import Flask, jsonify, render_template;
+from threading import Thread;
+
+import func;
+
+doLoop = True;
+
+serInput = serial.Serial('/dev/ttyUSB0',9600,timeout=0.1);
+serInput.flushInput();
+
+app = Flask(__name__);
+
+def check_for_notification():
+    func.log('main.py', 'check_for_notification', 'start');
+    rec_buff = '';
+    try:
+        time.sleep(0.25);
+        if serInput.inWaiting():
+            time.sleep(0.01);
+            rec_buff = serInput.read(serInput.inWaiting());
+        resp = str(rec_buff.decode());
+
+        if len(resp) > 0:
+            if "S" in rec_buff.decode():
+                vardummy = "";
+            elif "H" in rec_buff.decode():
+                vardummy = "";
+            else:
+                vardummy = "";
+            func.log('main.py', 'check_for_notification', 'rec_buff: ' + rec_buff.decode());
+    except:
+        func.log('main.py', 'check_for_notification', 'Exception (' + str(sys.exc_info()[0]) + ') has been caught.');
+
+    func.log('main.py', 'check_for_notification', 'end');
+    return resp;
+
+def shutdown():
+    currentLine1 = "Shutting down...";
+    time.sleep(5);
+    os.system("sudo shutdown now");
+
+@app.route('/')
+def index():
+    #disp.display_text("Index hit");
+    return render_template('index.html');
+
+@app.route('/shutdown/')
+def flask_shutdown():
+    shutdown();
+    mybody = "Shutdown initiated";
+    resp_obj = {
+        'status': "SUCCESS",
+        'body': mybody
+        }
+    return jsonify(resp_obj);
+
+@app.route('/service/jsontest/')
+def flask_jsontest():
+    mybody = "This is my json test";
+    resp_obj = {
+        'status': "SUCCESS",
+        'body': mybody
+        }
+    return jsonify(resp_obj);
+
+def main_loop():
+    global doLoop;
+    time_updates = time.time();
+    while doLoop:
+        try:
+            #func.log('main.py', 'myloop', 'looping...');s
+            if (time.time() - time_updates > 5):
+                check_sim_notification();
+        except:
+            func.log('main.py', 'main_loop', 'Exception: ' + str(sys.exc_info()));
+
+def start_flask():
+    try:
+        func.log('main.py', 'start_flask', 'Flask running');
+        app.run(debug=False, host='0.0.0.0');
+    except:
+        func.log('main.py', 'start_flask', 'Exception: ' + str(sys.exc_info()));
+
+if __name__ == '__main__':
+    try:
+        thread1 = Thread(target=main_loop);
+        thread2 = Thread(target=start_flask);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+    except :
+        func.log('main.py', '__main__', 'Exception (ID: {}) has been caught. Cleaning up...'.format(signal));
+        exit(0);
