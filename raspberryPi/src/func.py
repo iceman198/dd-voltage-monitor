253948140627d11e28969f7f53c1b5c0bd5a29bc
Sean Dutton <iceman198@gmail.com>
2021-10-08 11:23:27 -0400

    can't spell

--- raspberryPi/src/func.py
+++ raspberryPi/src/func.py
@@ -11,7 +11,7 @@ def log(file, service, text):
     f.write(mystring+'\r\n');
 
 def log_voltage(v1, v2, v3, v4):
-    f = open("/home/pi/share/volageLog.csv", "a");
+    f = open("/home/pi/share/voltageLog.csv", "a");
     now = datetime.datetime.now();
     mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ',' + str(v1) + ',' + str(v2) + ',' + str(v3) + ',' + str(v4);
     f.write(mystring+'\r\n');
