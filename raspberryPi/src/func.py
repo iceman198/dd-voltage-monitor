bed42d4408e1d797833efbd4b228a1881bf3e4e6
Sean Dutton <iceman198@gmail.com>
2021-10-10 13:54:21 -0400

    better logging

--- raspberryPi/src/func.py
+++ raspberryPi/src/func.py
@@ -1,18 +1,35 @@
 import datetime;
+import os;
+
+
+logging_path = "/home/pi/share/";
+log_file_name = "voltageMonitor.log";
+voltage_log_name = "voltageLog.csv";
+
+file_size_max_mb = 2;
 
 def print_test():
     print('This is a test');
 
-def log(file, service, text):
-    f = open("/home/pi/share/voltageMonitor.log", "a");
+def log(myclass, service, text):
+    update_file_if_needed(logging_path, log_file_name);
+    f = open(logging_path + log_file_name, "a");
     now = datetime.datetime.now();
-    mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ' *** ' + str(file) + ' *** ' + str(service) + '() ~ ' + str(text);
+    mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ' *** ' + str(myclass) + ' *** ' + str(service) + '() ~ ' + str(text);
     print(mystring);
     f.write(mystring+'\r\n');
 
 def log_voltage(v1, v2, v3, v4):
-    f = open("/home/pi/share/voltageLog.csv", "a");
+    update_file_if_needed(logging_path, voltage_log_name);
+    f = open(logging_path + voltage_log_name, "a");
     now = datetime.datetime.now();
     mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ',' + str(v1) + ',' + str(v2) + ',' + str(v3) + ',' + str(v4);
     f.write(mystring+'\r\n');
 
+def update_file_if_needed(filepath, oldfilename):
+    file_size = os.path.getsize(filepath + oldfilename);
+    size_in_mb = file_size/1024**2;
+    if (size_in_mb > file_size_max_mb):
+        now = datetime.datetime.now();
+        new_file_name = oldfilename + str(now.strftime('%Y-%m-%d_%H%M%S'));
+        os.rename(filepath + oldfilename, filepath + new_file_name);
