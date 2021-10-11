20815abd500f1d8f36d12006a36371f92f96fd41
Sean Dutton <iceman198@gmail.com>
2021-10-11 15:37:55 -0400

    m

--- raspberryPi/src/func.py
+++ raspberryPi/src/func.py
@@ -57,114 +57,110 @@ def get_history():
     file = open(myfilename,'r');
     lines = file.readlines(); #2021-10-11 13:34:28,13.032547808773282,566,13.078599214457993,568,23.555294007729803,1023,11.950339775182568,519
 
-    if (len(lines) > n):
-        i = 0;
-        line_avg_a1, line_avg_a2, line_avg_a3, line_avg_a4, line_avg_v1, line_avg_v2, line_avg_v3, line_avg_v4 = [0]*8;
-        line_max_a1, line_max_a2, line_max_a3, line_max_a4, line_max_v1, line_max_v2, line_max_v3, line_max_v4 = [0]*8;
-        line_min_a1, line_min_a2, line_min_a3, line_min_a4, line_min_v1, line_min_v2, line_min_v3, line_min_v4 = [1000]*8;
-        while(n > 0):
-            line = lines[len(lines) - n];
-            lineArr = line.split(',');
+    if (len(lines) < n): n = len(lines);
+    i = 0;
+    line_avg_a1, line_avg_a2, line_avg_a3, line_avg_a4, line_avg_v1, line_avg_v2, line_avg_v3, line_avg_v4 = [0]*8;
+    line_max_a1, line_max_a2, line_max_a3, line_max_a4, line_max_v1, line_max_v2, line_max_v3, line_max_v4 = [0]*8;
+    line_min_a1, line_min_a2, line_min_a3, line_min_a4, line_min_v1, line_min_v2, line_min_v3, line_min_v4 = [1000]*8;
+    while(n > 0):
+        line = lines[len(lines) - n];
+        lineArr = line.split(',');
 
-            if float(lineArr[1]) > line_max_v1: line_max_v1 = float(lineArr[1]);
-            if float(lineArr[1]) < line_min_v1: line_min_v1 = float(lineArr[1]);
-            line_avg_v1 = line_avg_v1 + float(lineArr[1]);
+        if float(lineArr[1]) > line_max_v1: line_max_v1 = float(lineArr[1]);
+        if float(lineArr[1]) < line_min_v1: line_min_v1 = float(lineArr[1]);
+        line_avg_v1 = line_avg_v1 + float(lineArr[1]);
 
-            if float(lineArr[2]) > line_max_a1: line_max_a1 = float(lineArr[2]);
-            if float(lineArr[2]) < line_min_a1: line_min_a1 = float(lineArr[2]);
-            line_avg_a1 = line_avg_a1 + float(lineArr[2]);
-            #print('line_avg_a1: ' + str(line_avg_a1) + ' ## lineArr2: ' + str(lineArr[2]));
+        if float(lineArr[2]) > line_max_a1: line_max_a1 = float(lineArr[2]);
+        if float(lineArr[2]) < line_min_a1: line_min_a1 = float(lineArr[2]);
+        line_avg_a1 = line_avg_a1 + float(lineArr[2]);
+        #print('line_avg_a1: ' + str(line_avg_a1) + ' ## lineArr2: ' + str(lineArr[2]));
 
-            if float(lineArr[3]) > line_max_v2: line_max_v2 = float(lineArr[3]);
-            if float(lineArr[3]) < line_min_v2: line_min_v2 = float(lineArr[3]);
-            line_avg_v2 = line_avg_v2 + float(lineArr[3]);
+        if float(lineArr[3]) > line_max_v2: line_max_v2 = float(lineArr[3]);
+        if float(lineArr[3]) < line_min_v2: line_min_v2 = float(lineArr[3]);
+        line_avg_v2 = line_avg_v2 + float(lineArr[3]);
 
-            if float(lineArr[4]) > line_max_a2: line_max_a2 = float(lineArr[4]);
-            if float(lineArr[4]) < line_min_a2: line_min_a2 = float(lineArr[4]);
-            line_avg_a2 = line_avg_a2 + float(lineArr[4]);
+        if float(lineArr[4]) > line_max_a2: line_max_a2 = float(lineArr[4]);
+        if float(lineArr[4]) < line_min_a2: line_min_a2 = float(lineArr[4]);
+        line_avg_a2 = line_avg_a2 + float(lineArr[4]);
 
-            if float(lineArr[5]) > line_max_v3: line_max_v3 = float(lineArr[5]);
-            if float(lineArr[5]) < line_min_v3: line_min_v3 = float(lineArr[5]);
-            line_avg_v3 = line_avg_v3 + float(lineArr[5]);
+        if float(lineArr[5]) > line_max_v3: line_max_v3 = float(lineArr[5]);
+        if float(lineArr[5]) < line_min_v3: line_min_v3 = float(lineArr[5]);
+        line_avg_v3 = line_avg_v3 + float(lineArr[5]);
 
-            if float(lineArr[6]) > line_max_a3: line_max_a3 = float(lineArr[6]);
-            if float(lineArr[6]) < line_min_a3: line_min_a3 = float(lineArr[6]);
-            line_avg_a3 = line_avg_a2 + float(lineArr[6]);
+        if float(lineArr[6]) > line_max_a3: line_max_a3 = float(lineArr[6]);
+        if float(lineArr[6]) < line_min_a3: line_min_a3 = float(lineArr[6]);
+        line_avg_a3 = line_avg_a2 + float(lineArr[6]);
 
-            if float(lineArr[7]) > line_max_v4: line_max_v4 = float(lineArr[7]);
-            if float(lineArr[7]) < line_min_v4: line_min_v4 = float(lineArr[7]);
-            line_avg_v4 = line_avg_v4 + float(lineArr[7]);
+        if float(lineArr[7]) > line_max_v4: line_max_v4 = float(lineArr[7]);
+        if float(lineArr[7]) < line_min_v4: line_min_v4 = float(lineArr[7]);
+        line_avg_v4 = line_avg_v4 + float(lineArr[7]);
 
-            if float(lineArr[8]) > line_max_a4: line_max_a4 = float(lineArr[8]);
-            if float(lineArr[8]) < line_min_a4: line_min_a4 = float(lineArr[8]);
-            line_avg_a4 = line_avg_a4 + float(lineArr[8]);
-            
-            i = i + 1;
-            if (i > lineskips):
-                line_avg_a1 = line_avg_a1 / i;
-                line_avg_a2 = line_avg_a2 / i;
-                line_avg_a3 = line_avg_a3 / i;
-                line_avg_a4 = line_avg_a4 / i;
-                line_avg_v1 = line_avg_v1 / i;
-                line_avg_v2 = line_avg_v2 / i;
-                line_avg_v3 = line_avg_v3 / i;
-                line_avg_v4 = line_avg_v4 / i;
-                i = 0;
-                returnlines.append({
-                    'timestamp': lineArr[0],
-                    'a1': {
-                        'min': line_min_a1,
-                        'max': line_max_a1,
-                        'avg': line_avg_a1
-                        },
-                    'a2': {
-                        'min': line_min_a2,
-                        'max': line_max_a2,
-                        'avg': line_avg_a2
-                        },
-                    'a3': {
-                        'min': line_min_a3,
-                        'max': line_max_a3,
-                        'avg': line_avg_a3
-                        },
-                    'a4': {
-                        'min': line_min_a4,
-                        'max': line_max_a4,
-                        'avg': line_avg_a4
-                        },
-                    'v1': {
-                        'min': line_min_v1,
-                        'max': line_max_v1,
-                        'avg': line_avg_v1
-                        },
-                    'v2': {
-                        'min': line_min_v2,
-                        'max': line_max_v2,
-                        'avg': line_avg_v2
-                        },
-                    'v3': {
-                        'min': line_min_v3,
-                        'max': line_max_v3,
-                        'avg': line_avg_v3
-                        },
-                    'v4': {
-                        'min': line_min_v4,
-                        'max': line_max_v4,
-                        'avg': line_avg_v4
-                        }
-                });
-                line_avg_a1, line_avg_a2, line_avg_a3, line_avg_a4, line_avg_v1, line_avg_v2, line_avg_v3, line_avg_v4 = [0]*8;
-                line_max_a1, line_max_a2, line_max_a3, line_max_a4, line_max_v1, line_max_v2, line_max_v3, line_max_v4 = [0]*8;
-                line_min_a1, line_min_a2, line_min_a3, line_min_a4, line_min_v1, line_min_v2, line_min_v3, line_min_v4 = [1000]*8;
+        if float(lineArr[8]) > line_max_a4: line_max_a4 = float(lineArr[8]);
+        if float(lineArr[8]) < line_min_a4: line_min_a4 = float(lineArr[8]);
+        line_avg_a4 = line_avg_a4 + float(lineArr[8]);
+        
+        i = i + 1;
+        if (i > lineskips | n <= 1):
+            line_avg_a1 = line_avg_a1 / i;
+            line_avg_a2 = line_avg_a2 / i;
+            line_avg_a3 = line_avg_a3 / i;
+            line_avg_a4 = line_avg_a4 / i;
+            line_avg_v1 = line_avg_v1 / i;
+            line_avg_v2 = line_avg_v2 / i;
+            line_avg_v3 = line_avg_v3 / i;
+            line_avg_v4 = line_avg_v4 / i;
+            i = 0;
+            returnlines.append({
+                'timestamp': lineArr[0],
+                'a1': {
+                    'min': line_min_a1,
+                    'max': line_max_a1,
+                    'avg': line_avg_a1
+                    },
+                'a2': {
+                    'min': line_min_a2,
+                    'max': line_max_a2,
+                    'avg': line_avg_a2
+                    },
+                'a3': {
+                    'min': line_min_a3,
+                    'max': line_max_a3,
+                    'avg': line_avg_a3
+                    },
+                'a4': {
+                    'min': line_min_a4,
+                    'max': line_max_a4,
+                    'avg': line_avg_a4
+                    },
+                'v1': {
+                    'min': line_min_v1,
+                    'max': line_max_v1,
+                    'avg': line_avg_v1
+                    },
+                'v2': {
+                    'min': line_min_v2,
+                    'max': line_max_v2,
+                    'avg': line_avg_v2
+                    },
+                'v3': {
+                    'min': line_min_v3,
+                    'max': line_max_v3,
+                    'avg': line_avg_v3
+                    },
+                'v4': {
+                    'min': line_min_v4,
+                    'max': line_max_v4,
+                    'avg': line_avg_v4
+                    }
+            });
+            line_avg_a1, line_avg_a2, line_avg_a3, line_avg_a4, line_avg_v1, line_avg_v2, line_avg_v3, line_avg_v4 = [0]*8;
+            line_max_a1, line_max_a2, line_max_a3, line_max_a4, line_max_v1, line_max_v2, line_max_v3, line_max_v4 = [0]*8;
+            line_min_a1, line_min_a2, line_min_a3, line_min_a4, line_min_v1, line_min_v2, line_min_v3, line_min_v4 = [1000]*8;
 
-            newlines.append(lines[len(lines) - n]);
-            n = n - 1;
-    else:
-        newlines = lines;
+        newlines.append(lines[len(lines) - n]);
+        n = n - 1;
 
     return returnlines;
-
-
 
     #if (len(lines) > n):
     #    while(n > 0):
