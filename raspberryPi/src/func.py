350b7dace7124011809b300c80c2b3fca887b2b9
Sean Dutton <iceman198@gmail.com>
2021-10-11 16:28:19 -0400

    m

--- raspberryPi/src/func.py
+++ raspberryPi/src/func.py
@@ -49,7 +49,7 @@ def verify_file(filewithpath):
 
 def get_history():
     #print('get_history start');
-    n = 8000;
+    n = 12000;
     lineskips = 300;
     myfilename = logging_path + voltage_log_name;
     newlines = [];
