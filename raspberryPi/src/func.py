90c885c09a9cd2f9e0d2eb75aa8891e15829bf9f
Sean Dutton <iceman198@gmail.com>
2021-10-11 08:47:55 -0400

    m

--- raspberryPi/src/func.py
+++ raspberryPi/src/func.py
@@ -12,19 +12,23 @@ def print_test():
     print('This is a test');
 
 def log(myclass, service, text):
+    verify_file(logging_path + log_file_name);
     update_file_if_needed(logging_path, log_file_name);
     f = open(logging_path + log_file_name, "a");
     now = datetime.datetime.now();
     mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ' *** ' + str(myclass) + ' *** ' + str(service) + '() ~ ' + str(text);
     print(mystring);
     f.write(mystring+'\r\n');
+    f.close();
 
 def log_voltage(v1, a1, v2, a2, v3, a3, v4, a4):
+    verify_file(logging_path + voltage_log_name);
     update_file_if_needed(logging_path, voltage_log_name);
     f = open(logging_path + voltage_log_name, "a");
     now = datetime.datetime.now();
     mystring = str(now.strftime('%Y-%m-%d %H:%M:%S')) + ',' + str(v1) + ',' + str(a1) + ',' + str(v2) + ',' + str(a2) + ',' + str(v3) + ',' + str(a3) + ',' + str(v4) + ',' + str(a4);
     f.write(mystring+'\n');
+    f.close();
 
 def update_file_if_needed(filepath, oldfilename):
     file_size = os.path.getsize(filepath + oldfilename);
@@ -33,6 +37,15 @@ def update_file_if_needed(filepath, oldfilename):
         now = datetime.datetime.now();
         new_file_name = oldfilename + str(now.strftime('%Y-%m-%d_%H%M%S'));
         os.rename(filepath + oldfilename, filepath + new_file_name);
+        verify_file(filepath + oldfilename);
+
+def verify_file(filewithpath):
+    try:
+        f = open(filewithpath);
+        f.close();
+    except FileNotFoundError:
+        f = open(filewithpath,"a");
+        f.close();
 
 def get_history():
     #print('get_history start');
--- raspberryPi/src/test.py
+++ raspberryPi/src/test.py
@@ -1,6 +1,15 @@
-R1 = 30000.0;
-R2 = 7500.0;
-value = 545;
+value = 552;
+
+R1 = 27730.0;
+R2 = 7463.0;
+
+vOUT = (value * 5.0) / 1024.0;
+vIN = vOUT / (R2/(R1+R2));
+print(vIN);
+
+
+value = 553;
+
 vOUT = (value * 5.0) / 1024.0;
 vIN = vOUT / (R2/(R1+R2));
 print(vIN);
