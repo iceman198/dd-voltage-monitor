eb67ade8403e0906e9616b7e71e686eb68eea1c7
Sean Dutton <iceman198@gmail.com>
2021-10-11 09:24:12 -0400

    m

--- raspberryPi/src/main.py
+++ raspberryPi/src/main.py
@@ -70,7 +70,7 @@ def check_for_notification():
 def calculate_voltage(value):
     R1 = 27730.0;
     R2 = 7463.0;
-    vOUT = (value * 5.0) / 1024.0;
+    vOUT = (float(value) * 5.0) / 1024.0;
     vIN = vOUT / (R2/(R1+R2));
     return vIN;
 
