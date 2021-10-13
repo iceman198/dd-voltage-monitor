123db6b0df78b9b959815e5022e67385bf954ccd
Sean Dutton <iceman198@gmail.com>
2021-10-13 15:07:24 -0400

    m

--- raspberryPi/src/test.py
+++ raspberryPi/src/test.py
@@ -1,15 +1,15 @@
-value = 552;
-
 R1 = 27730.0;
 R2 = 7463.0;
 
+value = 552; # should be 12.7
+value = 558; # should be 12.83
+value = 566; # should be 13.02
 vOUT = (value * 5.0) / 1024.0;
 vIN = vOUT / (R2/(R1+R2));
 print('try one: ' + str(vIN));
 
 
 value = 553;
-
 vOUT = (value * 5.0) / 1024.0;
 vIN = vOUT / (R2/(R1+R2));
 print('try two: ' + str(vIN));
